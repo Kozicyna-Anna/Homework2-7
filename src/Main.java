b8427d3dee09e06910d94748d56548e48b97654f
New <Dar1985@bk.ru>
2024-07-30 23:51:36 +0700

    Проверить ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.*;
 public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    public static void tack1() {
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+    public static void tack2() {
+        Collections.sort(nums);
+        int prevNum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.println(num);
+                prevNum = num;
+
+            }
+        }
+    }
+    public static void tack3() {
+        Set<String> uniqueWords = new HashSet<>(strings);
+        System.out.println(uniqueWords);
+    }
+    public static void tack4() {
+      Set<String> uniqueWords = new HashSet<>(strings);
+      System.out.println(strings.size() - uniqueWords.size());
     }
 }
+
+
